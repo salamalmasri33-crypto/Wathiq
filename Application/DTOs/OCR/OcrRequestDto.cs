154ff8e3504719ea0c1ea815fbd3a059c3c9f5be
Salam <salamalmasri33@gmail.com>
2025-12-23 18:06:12 +0300

    Add OCR request DTO

---
+++ Application/DTOs/OCR/OcrRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace eArchiveSystem.Application.DTOs.OCR
+{
+    public class OcrRequestDto
+    {
+        public IFormFile File { get; set; }
+        public string Language { get; set; } = "ara+eng";
+    }
+}
