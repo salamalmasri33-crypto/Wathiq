20e14ffbb3241a2026071ae3efe4bab32d419a7a
Najat-bostaty <nanabostaty2003@gmail.com>
2026-01-02 17:24:19 +0300

    Add Wathiq FrontEnd junior project

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5280/api";
+const API_BASE_URL = "https://localhost:44302/api";
 
 // نجهز instance من axios فيه كل الإعدادات
 const api = axios.create({
